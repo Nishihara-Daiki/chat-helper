33e787681d46fe40170d5285004294db50d83f23
nishihara-daiki <e1227@ob.akashi.ac.jp>
2021-08-02 00:39:21 +0900

    add function of copying message text

--- src/content_scripts/content.js
+++ src/content_scripts/content.js
@@ -146,6 +146,13 @@ var put_pinned_banner = e => {
 	});
 };
 
+// メッセージコピーボタン
+var put_copy_button = e => {
+	var text = e.querySelector('div[jsname="bgckF"]').textContent;
+	var icon_svg = '<svg viewBox="0 0 24 24" class=" f8lxbf waxfdf ZnfIwf"><path d="M0 0h24v24H0z" fill="none"></path><path d="M16 1H4c-1.1 0-2 .9-2 2v14h2V3h12V1zm3 4H8c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h11c1.1 0 2-.9 2-2V7c0-1.1-.9-2-2-2zm0 16H8V7h11v14z"></path></svg>';
+	put_message_action_button(e, icon_svg, 'メッセージをコピー', e => {execCopy(text)});
+};
+
 // 画像を閉じる
 var hold_images = (e, is_open) => {
 	if (e.hasChildNodes() == false) {
@@ -301,6 +308,10 @@ var main = () => {
 
 			if (settings["pin_message"]) {
 				put_pinned_button(e);
+			}
+
+			if (settings["copy_message"]) {
+				put_copy_button(e);
 			}
 
 			if (settings["markdown"]) {
